d2f701e3f95d2710193ad7cfd62ea24a8ab210ee
Chelpot <cheltest123@gmail.com>
2023-05-16 02:50:35 +0200

    create some models/main/repos for mvc

---
+++ main.py
@@ -0,0 +1,10 @@
+from view import globalView as gb
+
+def main():
+    gb.displayWelcomingMessage()
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ view/globalView.py
@@ -0,0 +1,6 @@
+def displayWelcomingMessage():
+    print("*************************************************")
+    print("*   Bienvenue, vous utilisez actuellement un    *")
+    print("*  logiciel permettant de gérer les tournois du *")
+    print("*                 club d'échec.                 *")
+    print("*************************************************")
