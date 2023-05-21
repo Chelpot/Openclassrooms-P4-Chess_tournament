aea9bcb0104e35cf3de2603ea9f45ba5f9357c0f
Chelpot <cheltest123@gmail.com>
2023-05-22 00:12:16 +0200

    Fix typo

--- models/player.py
+++ models/player.py
@@ -1,8 +1,8 @@
 class Player:
-    """A chest player"""
-    def __init__(self, first_name, last_name, birth_date, national_chest_id):
+    """A chess player"""
+    def __init__(self, first_name, last_name, birth_date, national_chess_id):
         self.first_name = first_name,
         self.last_name = last_name,
         self.birth_date = birth_date,
-        self.national_chest_id = national_chest_id
+        self.national_chess_id = national_chess_id
         
